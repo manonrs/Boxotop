e7112e0126ee7a0d03c5cd89b03ac2482e745930
manonrusso <manon.russo@appsolute.fr>
2024-05-24 17:15:47 +0200

    init project

---
+++ Boxotop/Boxotop/BoxotopApp.swift
@@ -0,0 +1,20 @@
+//
+//  BoxotopApp.swift
+//  Boxotop
+//
+//  Created by Manon Russo  on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BoxotopApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
